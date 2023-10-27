bd1b1e88275e9a7589840ed7d6cb1375d64e2085
Khushal sadashiv Bhamre <126353661+khushal-bhamre@users.noreply.github.com>
2023-10-27 20:22:40 +0530

    Create Variables.java

---
+++ Week_1/Variables.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Variables{
+    public static void main(String[] args) {
+        //datatypes 
+       //initialization,declaration 
+        int age=20;
+        double cgpa=7.7;
+        char first='K';
+        boolean breakfast=false;
+        System.out.println(age);
+        System.out.println(cgpa);
+        System.out.println(first);
+        System.out.println(breakfast);
+    }
+}
