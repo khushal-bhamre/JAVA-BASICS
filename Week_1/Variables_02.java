2c53b86fba4ec821e0fe0954271e44428304f28a
Khushal sadashiv Bhamre <126353661+khushal-bhamre@users.noreply.github.com>
2023-10-30 23:07:20 +0530

    Update and rename Variables.java to Variables_02.java

--- Week_1/Variables_02.java
+++ Week_1/Variables_02.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Variables{
+public class Variables_02{
     public static void main(String[] args) {
         //datatypes 
        //initialization,declaration 
