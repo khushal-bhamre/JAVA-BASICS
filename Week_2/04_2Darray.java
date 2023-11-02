d33ebecf37de6deea82b68afb824f05bd5429f5f
Khushal sadashiv Bhamre <126353661+khushal-bhamre@users.noreply.github.com>
2023-11-02 23:47:34 +0530

    Create 04_2Darray.java

---
+++ Week_2/04_2Darray.java
@@ -0,0 +1,14 @@
+public class04_2Darray{
+public static void main(String[] args){
+  int [][]arr=new int[3][3];  //declaration
+
+     arr[0][0]=1;
+     arr[0][1]=1;
+     arr[0][2]=1;      //assign values
+
+      System.out.println(arr[0][0]);
+      System.out.println(arr[0][1]);
+      System.out.println(arr[0][2]);  //accesing values
+ 
+  }
+}
